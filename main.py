f33e97cbacdbf8784f3de3ebeb426afae9d569d8
judah-levi <kolamiti92@gmail.com>
2020-10-19 10:44:04 +0300

    upadated error handling

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, File, UploadFile, HTTPException
 app = FastAPI()
 
 
 @app.post("/upload")
 def upload_file(file: UploadFile=File(...)):
     filename = file.filename
-    if file:
+    try: 
         with open(f'./tmp/{filename}', 'wb') as f:
             [f.write(chunk) for chunk in iter(lambda: file.file.read(10240), b'')]
         return {
                     "message": "File Uploaded successfully",
                     "filename": filename
                 }
-    else:
-        return {"message": "File Uploaded failed"}
+    except:
+        raise HTTPException(status_code=400, detail="File upload failed, did you include a file in your req?")
